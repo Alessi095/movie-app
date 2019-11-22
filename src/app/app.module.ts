3c8ce250871c0d67a1e4cfb81c4556e0a3e82d04
AlessioOliviero95 <Alessioliviero95@gmail.com>
2019-11-22 16:05:24 +0100

    boiler-plate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
+import { DetailMovieComponent } from './detail-movie/detail-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    ListMoviesComponent,
+    DetailMovieComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/movies-detail.ts
@@ -0,0 +1,19 @@
+export class MoviesDetail{
+   
+   
+    Title: string;
+    Year: string;
+    Rated:string;
+    Released:string
+    Runtime:string 
+    Genre: string
+    Directorstring:string
+    Writer: string
+    Actors: string
+    Plot: string
+    Language: string
+    Country: string
+    Awards:string
+    Poster: string
+
+}
